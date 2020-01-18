fcb78e5352229179efd51b89ae74a63717aa5e3b
RedEyeDragon <bjduarte@asu.edu>
2020-01-18 08:30:02 -0700

    stock tracker code

--- StockTracker.py
+++ StockTracker.py
@@ -37,64 +37,34 @@ class StockTracker:
       self.investment_list.append(symbol)
       self.watch_list.append(symbol)
       self.investment_list.sort()
-      self.investment_list.sort()
+      self.watch_list.sort()
     else:
       self.watch_list.append(symbol)
       self.watch_list.sort()
     
 
 # removes a stock ticker symbol from the list
-
   def remove_ticker(self, symbol, my_list = False):
     if my_list == True:
       self.investment_list.remove(symbol)
     else:
       self.watch_list.remove(symbol)
 
-
-# constructs the Yahoo Finance URL to pull stock data
-# param is string of stock data to be pulled
-  def construct_url (self, params, company_list):
-    url = "http://download.finance.yahoo.com/d/filename?s="+company_list+"&f="+params+"&interval=1d&events=history&self.start_date, self.end_date, self.crumb"
-    print("URL called")
-    # response = urllib.request.urlopen(url)
-    response = requests.get(url)
-
-    data = csv.reader(io.TextIOWrapper(response))
-    print("data populated")
-
-    for row in data:
-      try:
-        print(row)
-        self.stock_name.append(row[0])
-        self.stock_price.append(float(row[1]))
-        self.date.append(row[2])
-        self.time.append(row[3])
-        self.percent_change.append(float(row[4]))
-        self.open.append(float(row[5]))
-        self.high.append(float(row[6]))
-        self.low.append(float(row[7]))
-    
-      except ValueError:
-        pass
-        return -0.1
-
   def build_stock_data(self):
-    for i in range( len(self.watch_list)):
-      self.stock_data = {'name': self.stock_name[i], 'price': self.stock_price[i],
-      'date': self.date[i], 'time': self.time[i],
-      'change': self.percent_change[i], 'open': self.open[i],
-      'high': self.high[i], 'low': self.low[i]}
-      
-      self.stocks.update({self.watch_list[i]: self.stock_data})
-      
+    for ticker in len(self.watch_list):
+      self.stock_data = {'name': self.stock_name[ticker], 'price': self.stock_price[ticker],
+      'date': self.date[ticker], 'time': self.time[ticker],
+      'change': self.percent_change[ticker], 'open': self.open[ticker],
+      'high': self.high[ticker], 'low': self.low[ticker]}
+
+      self.stocks.update({self.watch_list[ticker]: self.stock_data})
+
     print("Stocks \n")
     print(self.stocks)
-    
+
     #print(self.stocks["AAPL"]["time"]) 
-    
+
   def write_JSON(self):
     with open('stockPrices.json', 'w') as f:
       json.dump(self.stocks, f)
-      
-      
+
